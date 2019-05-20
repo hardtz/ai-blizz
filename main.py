ece13cd21a1086f6f0eaaa3c8994662a663bb5ec
Diego <50744920+hardtz@users.noreply.github.com>
2019-05-20 11:11:22 -0300

    1.0
    Showing question textbox and results window through wxPython frame

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 # Libraries
 import wolframalpha
 import wikipedia
+import wx
 
 # Variables
 app_id = "TT25E4-P949A9E2RG"
+app = wx.App()
+frm = wx.Frame(None, title="blizz")
 
 # Infinite loop
 while True:
-    question = input("May I help you? ")
+    # Question window
+    question = wx.TextEntryDialog(frm, 'May I help you?','blizz')
+    # Setting question default value
+    question.SetValue("")
+    if question.ShowModal() == wx.ID_OK:
+        print('You searched: %s\n' % question.GetValue())
 
     try:
         client = wolframalpha.Client(app_id)
-        res = client.query(question)
-        answer = next(res.results).text
+        res = client.query(question.GetValue())
 
-        # Print Wolframalpha result.
-        print(answer)
+        # Show Wolframalpha result.
+        wx.SafeShowMessage('Results', next(res.results).text)
 
     except:
         # Print Wikipedia summary result.
-        print(wikipedia.summary(question))
+        wx.SafeShowMessage('Results', wikipedia.summary(question.GetValue()))
+
+    question.Destroy()
