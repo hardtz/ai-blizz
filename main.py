ffe5ceaf18816b4ea09f0aee29212675a0ece7bf
Diego <50744920+lasarindiego@users.noreply.github.com>
2019-05-21 11:05:29 -0300

    1.1
    Fixed "traceback" issue and added more comments on the code
    Changed messages boxes

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
+"""
+
+WARNING: IF YOU DON'T CREATE A WOLFRAMALPHA ACCOUNT, AND EITHER DON'T ENTER YOUR APP_ID, THE APPLICATION
+WILL ONLY SEARCH ON WIKIPEDIA FOR RESULTS.
+
+"""
+
 # Libraries
 import wolframalpha
 import wikipedia
 import wx
 
 # Variables
-app_id = "TT25E4-P949A9E2RG"
+app_id = "YOUR_APP_ID"
 app = wx.App()
-frm = wx.Frame(None, title="blizz")
+frm = wx.Frame()
 
 # Infinite loop
 while True:
     # Question window
-    question = wx.TextEntryDialog(frm, 'May I help you?','blizz')
-    # Setting question default value
-    question.SetValue("")
+    question = wx.TextEntryDialog(frm, 'How can I help you?', 'blizz')
+
+    # Search if "Ok" was selected
     if question.ShowModal() == wx.ID_OK:
         print('You searched: %s\n' % question.GetValue())
 
-    try:
-        client = wolframalpha.Client(app_id)
-        res = client.query(question.GetValue())
+        # WolframAlpha is the first search option, in case the user wants to solve an equation
+        try:
+            # WolframAlpha client using your app_id
+            client = wolframalpha.Client(app_id)
 
-        # Show Wolframalpha result.
-        wx.SafeShowMessage('Results', next(res.results).text)
+            # Saves input query to be used as a parameter later
+            res = client.query(question.GetValue())
 
-    except:
-        # Print Wikipedia summary result.
-        wx.SafeShowMessage('Results', wikipedia.summary(question.GetValue()))
+            # Show WolframAlpha result
+            wx.MessageBox(next(res.results).text, 'Results')
+
+        # If WolframAlpha doesn't show any results, it will search on Wikipedia's summary
+        except:
+            # Print Wikipedia summary result
+            wx.MessageBox(wikipedia.summary(question.GetValue()), 'Results')
+
+    # Stops and close the application by clicking "Cancel" or X button
+    else:
+        break
 
     question.Destroy()
+
